78e4471488adce0462cb4d9b3a858ce766e40905
abhilashshettigartrt <abhilashshettigar.trt@gmail.com>
2018-02-14 18:00:06 +0530

    Caps specs.js

---
+++ helpers/desiredCapabilities.js
@@ -0,0 +1,13 @@
+exports.options = {
+    desiredCapabilities: {
+        "automationName": "Appium",
+        "platformName": "Android",
+        "platformVersion": "7.0",
+        "appPackage":"com.onemilliondream.app",
+        "appActivity":"com.onemilliondream.app.MainActivity",
+        "deviceName": "3300d05029f573ef",
+        "udid": "3300d05029f573ef",
+    },
+    host:'localhost',
+    port:4723
+};
